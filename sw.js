658e285f586d7ff407ef8afec935b19cedc5beb3
condaltosstaizabel <pafregestao@gmail.com>
2026-09-04 09:03:14 -0300

    ajuste

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'portal-asi-v15';
+const CACHE = 'portal-asi-v16';
 const ASSETS = ['./index.html','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
